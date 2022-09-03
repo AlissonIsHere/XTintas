6e357a3488907a6dbb6121190885b6f4ee24830b
Alisson <alissonalissonrodrigues@outlook.com>
2022-09-03 15:53:00 -0300

    metodo get

--- lib/Presenter/store_presenter.dart
+++ lib/Presenter/store_presenter.dart
@@ -1,5 +1,15 @@
+import 'package:app_xtintas/Model/Repositories/paint_options_repository.dart';
+import 'package:app_xtintas/Model/paint_options.dart';
 import 'package:flutter/cupertino.dart';
 
 class StorePresenter with ChangeNotifier {
-  
+  final PaintOptionsRepository paintOptionsRepository;
+  StorePresenter(this.paintOptionsRepository);
+  List<PaintOptions> listPaintOptions = [];
+
+  void getPaintOptions() async {
+    notifyListeners();
+    listPaintOptions = await paintOptionsRepository.getPaintOptions();
+    notifyListeners();
+  }
 }
