13fe00f394c8ec6ad6f6d78dce9294f3bd8b8c60
Alisson <alissonalissonrodrigues@outlook.com>
2022-09-03 15:09:21 -0300

    paginas de loja, carrinho e perfil

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:app_xtintas/View/Authentication/login_page.dart';
 import 'package:app_xtintas/View/Authentication/registration_page.dart';
 import 'package:app_xtintas/View/bottom_navigation_bar.dart';
-import 'package:app_xtintas/View/cart_page.dart';
-import 'package:app_xtintas/View/profile_page.dart';
-import 'package:app_xtintas/View/store_page.dart';
 import 'package:app_xtintas/View/tutorial_page.dart';
 import 'package:flutter/material.dart';
 
@@ -24,9 +21,6 @@ class MyApp extends StatelessWidget {
         '/': (context) => const LoginPage(),
         '/registration': (context) => const RegistrationPage(),
         '/tutorial': (context) => TutorialPage(),
-        '/store': (context) => StorePage(),
-        '/profile': (context) => ProfilePage(),
-        '/cart': (context) => CartPage(),
         '/navigationBar': (context) => BottomNavigationBarPage()
       },
     );
